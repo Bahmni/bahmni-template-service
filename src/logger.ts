8f3b7ab15a55f1e79d51b7fba941c24955e8764b
ravinder kabli <ravinder.kabli@thoughtworks.com>
2026-05-18 14:30:08 +0530

    BAH-4608|Added MPL v2 with HD license files

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://www.bahmni.org/license/mplv2hd.
+ *
+ * Copyright 2026. Thoughtworks. Thoughtworks is a registered trademark
+ * and the Thoughtworks graphic logo is a trademark of Thoughtworks Inc.
+ */
+
 const LEVELS = { trace: 0, debug: 1, info: 2, warn: 3, error: 4, fatal: 5 } as const;
 
 type Level = keyof typeof LEVELS;

af6d0b723f7ab6e9c436126d1b25a186f0cbe1a4
ravinder kabli <ravinder.kabli@thoughtworks.com>
2026-05-18 14:30:08 +0530

    BAH-4608|Added MPL v2 with HD license files

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://www.bahmni.org/license/mplv2hd.
+ *
+ * Copyright 2026. Thoughtworks. Thoughtworks is a registered trademark
+ * and the Thoughtworks graphic logo is a trademark of Thoughtworks Inc.
+ */
+
 export class AppError extends Error {
   constructor(message: string, public readonly statusCode: number) {
     super(message);
--- src/template/fhirPath.ts
+++ src/template/fhirPath.ts
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://www.bahmni.org/license/mplv2hd.
+ *
+ * Copyright 2026. Thoughtworks. Thoughtworks is a registered trademark
+ * and the Thoughtworks graphic logo is a trademark of Thoughtworks Inc.
+ */
+
 import fhirpath from 'fhirpath';
 import logger from '../logger';
 
